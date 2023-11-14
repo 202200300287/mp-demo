158c80ed0f92aa8374725b2e48563dcc4c354339
Huaisui-O-O <18866963501@163.com>
2023-11-14 21:43:21 +0800

    完成鉴权拦截器，完成拦截器配置，完成Token工具类

--- src/main/java/com/itheima/mp/filter/AuthHandlerInterceptor.java
+++ src/main/java/com/itheima/mp/filter/AuthHandlerInterceptor.java
@@ -1,17 +1,66 @@
 package com.itheima.mp.filter;
 
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itheima.mp.util.TokenUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
-public class AuthHandlerInterceptor {
+public class AuthHandlerInterceptor implements HandlerInterceptor {
     @Autowired
     TokenUtil tokenUtil;
 
-    @Value("${token.youngToken}")
-    private Long youngToken;
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse response,Object handler) throws IOException {
+        log.info("============进入拦截器=============");
+        System.out.println(httpServletRequest.getHeader("User-Agent"));
+        if(!(handler instanceof HandlerMethod)) {
+            response.setStatus(401);
+            return false;
+        }
+        String token = httpServletRequest.getHeader("Authorization");
+        log.info("=============token:" + token);
+        Map<String,String> map = new HashMap<>();
+        try{
+            TokenUtil.verify(token);
+            return true;
+        }catch (SignatureVerificationException e) {
+            e.printStackTrace();
+            response.setStatus(401);
+            map.put("message","无效签名");
+        }catch (TokenExpiredException e) {
+            e.printStackTrace();
+            response.setStatus(401);
+            map.put("message","token过期");
+        }catch (AlgorithmMismatchException e) {
+            e.printStackTrace();
+            response.setStatus(401);
+            map.put("message","加密算法不匹配");
+        }catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(401);
+            map.put("message","token无效");
+        }
+        String json = new ObjectMapper().writeValueAsString(map);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().println(json);
+        return false;
+    }
+
+
 }
--- src/main/java/com/itheima/mp/util/TokenUtil.java
+++ src/main/java/com/itheima/mp/util/TokenUtil.java
@@ -9,7 +9,7 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.PrivateKey;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,29 +20,38 @@ import java.util.Map;
 @Component
 public class TokenUtil {
     @Value("${token.privateKey}")
-    String PrivateKey;
+    static String privateKey;
 
     //通过用户Id和用户名创建Token
-    public String getToken(Integer userId,String username) {
-        String token = JWT.create()
+    public String getToken(Integer userId,String username,Integer role) {
+        Calendar instance = Calendar.getInstance();
+        instance.add(Calendar.DATE,3);
+        return JWT.create()
                 .withClaim("userId",userId)
                 .withClaim("username",username)
-                .withClaim("timeStamp",System.currentTimeMillis())
-                .sign(Algorithm.HMAC256(PrivateKey));
-        return token;
+                .withClaim("role",role)
+                .withExpiresAt(instance.getTime())
+                .sign(Algorithm.HMAC256(privateKey));
+    }
+
+    public static void verify (String token) {
+        JWTVerifier verify = JWT.require(Algorithm.HMAC256(privateKey)).build();
+        verify.verify(token);
     }
 
 
     //解析Token工具方法
     public Map<String,String> parseToken(String token) throws JWTVerificationException {
         HashMap<String,String> map = new HashMap<>();
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(PrivateKey)).build();
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(privateKey)).build();
         DecodedJWT decodedJWT = verifier.verify(token);
         Claim userId = decodedJWT.getClaim("userId");
         Claim username = decodedJWT.getClaim("username");
+        Claim role = decodedJWT.getClaim("role");
         Claim timeStamp = decodedJWT.getClaim("timeStamp");
         map.put("userId",userId.asInt().toString());
         map.put("username",username.asString());
+        map.put("role",role.asInt().toString());
         map.put("timeStamp",timeStamp.asInt().toString());
         return map;
     }
